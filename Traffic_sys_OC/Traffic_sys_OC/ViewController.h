dfda519d69e18130d908614c9b510d69ed2fa8c4
易无解 <2198105373@qq.com>
2017-09-21 08:33:23 +0800

    创建项目

---
+++ Traffic_sys_OC/Traffic_sys_OC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Traffic_sys_OC
+//
+//  Created by 易无解 on 21/09/2017.
+//  Copyright © 2017 易无解. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Traffic_sys_OC/Traffic_sys_OC/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Traffic_sys_OC
+//
+//  Created by 易无解 on 21/09/2017.
+//  Copyright © 2017 易无解. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
